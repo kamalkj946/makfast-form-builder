705e8902356c039e66d830a18ef68f8aa8c9fad2
Piyush Dadheech <piyushdadheech786@gmail.com>
2026-04-17 16:49:08 +0530

    Stop auth route redirect loop by restoring authenticate.admin flow

--- app/routes/auth.$.tsx
+++ app/routes/auth.$.tsx
@@ -1,15 +1,7 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
-import { authenticate, login } from "../shopify.server";
+import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const url = new URL(request.url);
-  const normalizedPath = url.pathname.replace(/\/+$/, "");
-
-  if (normalizedPath === "/auth") {
-    await login(request);
-    return null;
-  }
-
   await authenticate.admin(request);
   return null;
 };

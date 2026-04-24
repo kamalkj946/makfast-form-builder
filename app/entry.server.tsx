e4e4200d801b991c4e147b6d4889885c4957ddf0
Piyush Dadheech <piyushdadheech786@gmail.com>
2026-04-24 13:53:13 +0530

    fix: force CSP frame-ancestors in entry.server to resolve refused connection

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -38,6 +38,10 @@ export default async function handleRequest(
           const stream = createReadableStreamFromReadable(body);
 
           responseHeaders.set("Content-Type", "text/html");
+          responseHeaders.set(
+            "Content-Security-Policy",
+            "frame-ancestors https://*.myshopify.com https://admin.shopify.com;"
+          );
           resolve(
             new Response(stream, {
               headers: responseHeaders,
